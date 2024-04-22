ecf2af272803239ed83bfdb0329ecf330779e8d4
Jianguo Ma <jiangma@microsoft.com>
2024-04-22 17:31:49 +0800

    remove ai service

--- src/main/java/org/springframework/samples/petclinic/DemoController.java
+++ src/main/java/org/springframework/samples/petclinic/DemoController.java
@@ -1,15 +1,5 @@
 package org.springframework.samples.petclinic;
 
-import dev.langchain4j.data.embedding.Embedding;
-import dev.langchain4j.data.segment.TextSegment;
-import dev.langchain4j.memory.ChatMemory;
-import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
-import dev.langchain4j.store.embedding.EmbeddingStore;
-import dev.langchain4j.service.AiServices;
-
 import org.springframework.samples.petclinic.model.Question;
 import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.samples.petclinic.owner.OwnerRepository;
@@ -26,31 +16,17 @@ import java.util.stream.Collectors;
 
 @Controller
 public class DemoController {
-    private final ChatLanguageModel chatLanguageModel;
-
-    private final EmbeddingModel embeddingModel;
-
-    private final EmbeddingStore<TextSegment> embeddingStore;
-
     private final OwnerRepository ownerRepository;
 
-
-    private Agent assistant = null;
     private final Question question = new Question();
-    
 
-    public DemoController(ChatLanguageModel chatLanguageModel, EmbeddingModel embeddingModel, EmbeddingStore<TextSegment> embeddingStore, OwnerRepository ownerRepository) {
-        this.chatLanguageModel = chatLanguageModel;
-        this.embeddingModel = embeddingModel;
-        this.embeddingStore = embeddingStore;
+    public DemoController(OwnerRepository ownerRepository) {
         this.ownerRepository = ownerRepository;
- 
     }
 
     @PostConstruct
     public void init() {
         embedOwners();
-        initChain();
     }
 
     private void embedOwners() {
@@ -62,26 +38,8 @@ public class DemoController {
                     "City: " + owner.getCity() + ", " +
                     "Telephone: " + owner.getTelephone() + ", " +
                     "Pets: " + owner.getPets().stream().map(Pet::getName).collect(Collectors.joining(", "));
-            TextSegment textSegment = TextSegment.from(ownerDetails);
-            Embedding embedding = embeddingModel.embed(ownerDetails).content();
-            embeddingStore.add(embedding, textSegment);
-
+            System.out.println(ownerDetails);
         }
-    }
-
-    private void initChain() {
-        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(20);
-
-        EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
-                .embeddingStore(embeddingStore)
-                .embeddingModel(embeddingModel)
-                .build();
-
-        assistant = AiServices.builder(Agent.class)
-                .chatLanguageModel(chatLanguageModel)
-                .chatMemory(chatMemory)
-                .contentRetriever(contentRetriever)
-                .build();
     }
 
     @ModelAttribute("question")
@@ -97,7 +55,7 @@ public class DemoController {
 
     @GetMapping("/aiChat/ask")
     String aiChatAsk(@ModelAttribute("question") Question question) {
-        question.setAnswer(assistant.chat(question.getQuestion()));
+        //question.setAnswer(assistant.chat(question.getQuestion()));
         return "redirect:/aiChat";
     }
 
